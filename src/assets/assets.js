5874d10ddd2bada816d609175ce5bae83af46788
email <jordanmorel.ca@gmail.com>
2024-10-29 23:21:42 -0500

    Avance de home_29-10-2024

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,6 +3,9 @@ import logo from '../assets/logo_fitsync-01.webp';
 import logo_Alternative from '../assets/logo_fitsync-02.webp';
 import phone_home from '../assets/phone_fitsync.png'
 import carrito from '../assets/carrito.svg'
-import account_login from '../assets/account-pin-circle-fill.svg'
+import account_login from '../assets/account-pin-circle-fill.svg';
+import promo_01 from '../assets/Inscripcion-gratis-fitsync.jpg';
+import promo_02 from '../assets/matricular-cero-con-fitsync.jpg';
+import promo_03 from '../assets/promo-suplementos-fitsync.jpg';
 
-export { logo, logo_Alternative, phone_home,carrito, account_login };
+export { logo, logo_Alternative, phone_home,carrito, account_login, promo_01, promo_02, promo_03 };
--- src/pages/HomeDesktop.jsx
+++ src/pages/HomeDesktop.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../sass/_homeDesktop.scss';
 import PromotionSlider from '../components/PromotionSlider';
-import {phone_home} from '../assets/assets.js'
+import {phone_home, promo_01, promo_02, promo_03} from '../assets/assets.js'
 
 const HomeDesktop = () => {
   return (
@@ -10,27 +10,25 @@ const HomeDesktop = () => {
       <section className="home__hero">
         <div className='home__hero--box'>
         <img src={phone_home} alt="FitSync Gym Image" className="home__phone-image" />
-        <h1>Controla tu entrenamiento en la palma de tu mano</h1>
+        <h1><spawn className="highcolor_Gold">Controla</spawn> tu entrenamiento en la palma de <spawn className="highcolor_Gold">tu mano</spawn></h1>
         </div>
       </section>
 
       <section className="home__services">
-        <PromotionSlider />
+        <PromotionSlider/>
         <button className="Service__cta">Contactar</button>
       </section>
       <section className='home__services'>
+      <h2 className='Promocion__title' >PROMOCIONES DEL MES</h2>
       <div className="services__list">
-          <div className="service">
-            <h3>Personal Training</h3>
-            <p>One-on-one sessions with our expert trainers</p>
+          <div className="service__box">
+          <img src={promo_01} alt="Inscripcion-en-fitsync" className="service__image" />
           </div>
-          <div className="service">
-            <h3>Group Classes</h3>
-            <p>Dynamic and engaging fitness classes</p>
+          <div className="service__box">
+            <img src={promo_02} alt="matricul-cero-en-fitsync" className="service__image" />
           </div>
-          <div className="service">
-            <h3>Nutrition Plans</h3>
-            <p>Customized plans to fit your goals</p>
+          <div className="service__box">
+          <img src={promo_03} alt="suplementos-fitsync" className="service__image" />
           </div>
         </div> 
       </section>
