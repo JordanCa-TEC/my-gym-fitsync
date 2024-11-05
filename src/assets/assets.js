80915f314bcaad8befd8c57194781e6c7776ebc4
email <jordanmorel.ca@gmail.com>
2024-11-04 22:17:10 -0500

    Arreglos al Footer_04-11-2024

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,14 @@
 import logo from '../assets/logo_fitsync-01.webp';
 import logo_Alternative from '../assets/logo_fitsync-02.webp';
 import phone_home from '../assets/phone_fitsync.png'
-import carrito from '../assets/carrito.svg'
+import carrito from '../assets/carrito.svg';
 import account_login from '../assets/account-pin-circle-fill.svg';
 import promo_01 from '../assets/Inscripcion-gratis-fitsync.jpg';
 import promo_02 from '../assets/matricular-cero-con-fitsync.jpg';
 import promo_03 from '../assets/promo-suplementos-fitsync.jpg';
+import facebookIcon from '../assets/facebook_fitsync.svg';
+import instagramIcon from '../assets/Instagram_fitsync.svg';
+import tiktokIcon from '../assets/tiktok_fitsync.svg';
+import whatsappIcon  from '../assets/whatsapp_fitsync.svg';	
 
-export { logo, logo_Alternative, phone_home,carrito, account_login, promo_01, promo_02, promo_03 };
+export { logo, logo_Alternative, phone_home, carrito, account_login, promo_01, promo_02, promo_03, facebookIcon, instagramIcon, tiktokIcon, whatsappIcon };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import '../sass/_Footer.scss'; 
+import '../sass/_Footer.scss';
+import { logo, whatsappIcon, tiktokIcon, instagramIcon, facebookIcon } from '../assets/assets.js';
 
 const Footer = () => {
   return (
     <footer className="footer">
-    <p>&copy; 2024 FitSync Gym</p>
-    <div className="footer__links">
-      <a href="/privacy">Privacy Policy</a>
-      <a href="/terms">Terms of Service</a>
-    </div>
-  </footer>
+      {/* Logo de FitSync */}
+      <div className="footer__logo">
+        <img src={logo} alt="FitSync Gym" />
+      </div>
+
+      {/* Enlaces principales */}
+      <div className="footer__links">
+        {/* Primera columna de enlaces */}
+        <section className="footer__links__row">
+          <h3>FitSync</h3>
+          <ul className="footer__menu">
+            <li><a href="/about">Quiénes somos</a></li>
+            <li><a href="/claims">Libro de reclamaciones</a></li>
+            <li><a href="/promotions">Promociones</a></li>
+          </ul>
+        </section>
+
+        {/* Segunda columna de enlaces */}
+        <section className="footer__links__row">
+          <h3>Planes</h3>
+          <ul className="footer__menu">
+            <li><a href="/plans">Conoce nuestros planes</a></li>
+            <li><a href="/documents">Documentos</a></li>
+          </ul>
+        </section>
+      </div>
+
+      {/* Íconos de redes sociales */}
+      <div className="footer__social">
+        <a href="#" aria-label="Facebook"><img src={facebookIcon} alt="Facebook" /></a>
+        <a href="#" aria-label="Instagram"><img src={instagramIcon} alt="Instagram" /></a>
+        <a href="#" aria-label="TikTok"><img src={tiktokIcon} alt="TikTok" /></a>
+        <a href="#" aria-label="WhatsApp"><img src={whatsappIcon} alt="WhatsApp" /></a>
+      </div>
+
+      {/* Texto de derechos reservados */}
+      <p>&copy; 2024 FitSync Gym - Derechos reservados</p>
+    </footer>
   );
 };
 
