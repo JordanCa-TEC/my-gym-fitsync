251ccc1e976ebc9f6f4e032f86373f5303cecef8
JordanCa-TEC <jordanmorel.ca@gmail.com>
2024-12-06 07:17:30 -0500

    Avance de pagina de contacto

--- src/pages/ContactDesktop.jsx
+++ src/pages/ContactDesktop.jsx
@@ -9,6 +9,10 @@ const ContactDesktop = () => {
       <h1>Contacto</h1>
     </section>
     <section>
+      <div className='info-container' >
+        <p>+51 992 721 688</p>
+        <p>servicioalcliente@fitsync.com</p>
+      </div>
     <div className='map-container'>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15606.216935274828!2d-77.0771852490112!3d-12.074161669380048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c970336b0725%3A0xae2a6f969b48786e!2sPlaza%20San%20Miguel!5e0!3m2!1ses!2spe!4v1732198547262!5m2!1ses!2spe"
