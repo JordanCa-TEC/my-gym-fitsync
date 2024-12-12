3ac208a3e378ac629b7aa6528c0d7e13d8b45358
email <jordanmorel.ca@gmail.com>
2024-12-11 22:01:06 -0500

    Maqueta para tienda de fitsync-11-12-2024

--- src/pages/ShopDesktop.jsx
+++ src/pages/ShopDesktop.jsx
@@ -7,21 +7,21 @@ const ShopDesktop = () => {
     <div className='shop'>
       <section className="shop__desktop">
                 <div className="parent">
-                  <div className="div1">
+                  <div className="shop__item--1 parent__content--div">
                     <img src={productHybro} alt="Hydro" />
                     <h2>Proteína Hidrolizada Hydrowhey 3.6lb – Optimum Nutrition</h2>
                     <h3>Proteínas</h3>
-                    <h2>s/.365.00</h2>
+                    <h2 className='item__price'>s/.365.00</h2>
                     <button>Agregar al carrito</button>
                   </div>
-                  <div className="div2"> </div>
-                  <div className="div3"> </div>
-                  <div className="div4"> </div>
-                  <div className="div5"> </div>
-                  <div className="div6"> </div>
-                  <div className="div7"> </div>
-                  <div className="div8"> </div>
-                  <div className="div9"> </div>
+                  <div className="shop__item--2 parent__content--div"> </div>
+                  <div className="shop__item--3 parent__content--div"> </div>
+                  <div className="shop__item--4 parent__content--div"> </div>
+                  <div className="shop__item--5 parent__content--div"> </div>
+                  <div className="shop__item--6 parent__content--div"> </div>
+                  <div className="shop__item--7 parent__content--div"> </div>
+                  <div className="shop__item--8 parent__content--div"> </div>
+                  <div className="shop__item--9 parent__content--div"> </div>
                 </div>
       </section>
     </div>
